e552d0bf37af7507a1f3db452760c2a80a0158cb
Bindi <bindin42@gmail.com>
2025-01-11 15:51:13 -0800

    animating witch

--- witch.js
+++ witch.js
@@ -1,7 +1,8 @@
 class Witch {
-
-    constructor() {
-
+    // witch frame in 38 h, 21 w
+    constructor(game) {
+        this.game = game;
+        this.animator = new Animator (ASSET_MANAGER.getAsset("./B_witch_idle.png"), 0, 0, 21, 38, 6, 0.2);
     };
 
     update() {
@@ -9,6 +10,6 @@ class Witch {
     };
 
     draw(ctx) {
-        ctx.drawImage(ASSET_MANAGER.getAsset("./B_witch_idle.png"), 0, 0)
+        this.animator.drawFrame(this.game.clockTick, ctx, 25, 25);
     };
 }
